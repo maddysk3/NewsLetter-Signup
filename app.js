8a57499620dbbb6d5e0029baf3994374d3b6af61
maddysk3 <53914850+maddysk3@users.noreply.github.com>
2023-06-16 12:26:35 -0400

    removed dotenv library

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
-require('dotenv').config()
 
 const app = express();
 app.use(express.static("public"));
